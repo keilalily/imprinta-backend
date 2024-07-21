785a6d8bf095bcad6f340b27ab5a05a42b3f4be4
Peter John <cabantogpeterjohnm@gmail.com>
2024-07-21 23:16:37 +0800

    may need dl na service key galing sa firebase, tapos need ayusin sa note pad, no error na pero not responding parin kahit click yung login

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
 const firebase= require('firebase');
+const admin = require('firebase-admin');
+const serviceAccount = require('D:/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://vpmm-9d033-default-rtdb.firebaseio.com/'
+});
 
 const firebaseConfig = {
     apiKey: "AIzaSyBejFI4CGdEAqnOC6z7FAYgamCFZEmHtE8",
@@ -9,6 +16,7 @@ const firebaseConfig = {
     appId: "1:1003758134447:web:520356eb40daab83ede05a",
     measurementId: "G-S6KZHFJQSH"
   };
+  
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const User = db.collection('users');
