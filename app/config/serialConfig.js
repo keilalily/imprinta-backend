0cb474183fbc32e4189db9a3b252bbedf1c94d55
unknown <kylamarieangeles@gmail.com>
2024-09-07 11:34:51 +0800

    Print Landscape

--- app/config/serialConfig.js
+++ app/config/serialConfig.js
@@ -1,11 +1,11 @@
-const { SerialPort } = require('serialport');
-const { ReadlineParser } = require('@serialport/parser-readline');
+// const { SerialPort } = require('serialport');
+// const { ReadlineParser } = require('@serialport/parser-readline');
 
-const port = new SerialPort({
-  path: 'COM5', // Replace with your Arduino's serial port
-  baudRate: 9600
-});
+// const port = new SerialPort({
+//   path: 'COM5', // Replace with your Arduino's serial port
+//   baudRate: 9600
+// });
 
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+// const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-module.exports = { port, parser };
+// module.exports = { port, parser };
